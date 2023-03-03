5fc9c9b3603185a17143f56cab13ddaff142ab50
Archer001201 <2111410956@qq.com>
2023-03-03 12:42:57 -0800

    重构了相机移动脚本

--- The Bullied and The Brave/Assets/Scripts/CemaraController.cs
+++ The Bullied and The Brave/Assets/Scripts/CemaraController.cs
@@ -4,44 +4,11 @@ using UnityEngine;
 
 public class CemaraController : MonoBehaviour
 {
-    public float speed;
-
-    private Rigidbody2D rb;
-    private float inputX, inputY;
-    private Vector2 movementInput;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        Movement();
-    }
-
     private void Update()
     {
-        //if (!photonView.IsMine && PhotonNetwork.IsConnected)
-        //    return;
-        PlayerInput();
-    }
-
-    private void PlayerInput()
-    {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
-
-        if (inputX != 0 && inputY != 0)
-        {
-            inputX *= 0.6f;
-            inputY *= 0.6f;
-        }
-        movementInput = new Vector2(inputX, inputY);
-    }
-
-    private void Movement()
-    {
-        rb.MovePosition(rb.position + movementInput * speed * Time.deltaTime);
+        GetComponent<Transform>().position = new Vector3(
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position.x,
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position.y,
+            -10);
     }
 }
