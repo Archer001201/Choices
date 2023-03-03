28aa20b0575eb6264fcd7dd3c554c0101a04fe65
Archer001201 <2111410956@qq.com>
2023-03-02 22:53:08 -0800

    角色&服务器创建
    1. 添加了角色行走图
    2. 服务器搭建完成
    3. 角色控制脚本（移动）

---
+++ The Bullied and The Brave/Assets/Scripts/Launcher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //调用Photon Server Settings
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //检测是否连接到游戏服务器
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        //创建房间
+        PhotonNetwork.JoinOrCreateRoom("IAT 222 Team 2-4", new Photon.Realtime.RoomOptions() { MaxPlayers = 2}, default);
+    }
+
+    //加入房间生成角色
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            PhotonNetwork.Instantiate("Player_1", new Vector3(0, 0, 0), Quaternion.identity, 0);
+        else if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+            PhotonNetwork.Instantiate("Player_2", new Vector3(5, 0, 0), Quaternion.identity, 0);
+    }
+}
---
+++ The Bullied and The Brave/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPun
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private float inputX, inputY;
+    private Vector2 movementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine && PhotonNetwork.IsConnected)
+            return;
+        PlayerInput();
+    }
+
+    private void PlayerInput()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        if (inputX != 0 && inputY != 0)
+        {
+            inputX *= 0.6f;
+            inputY *= 0.6f;
+        }
+        movementInput = new Vector2(inputX, inputY);
+    }
+
+    private void Movement()
+    {
+        rb.MovePosition(rb.position + movementInput * speed * Time.deltaTime);
+    }
+}
