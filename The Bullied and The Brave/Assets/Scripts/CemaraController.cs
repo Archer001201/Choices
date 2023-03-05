ff748b335a1f965a4ec6aee6b47139dfc2d3d7aa
Archer001201 <2111410956@qq.com>
2023-03-05 11:41:50 -0800

    对话UI
    1. 添加了一部分素材
    2. 创建了对话UI
    3. 修改了相机移动脚本bug

--- The Bullied and The Brave/Assets/Scripts/CemaraController.cs
+++ The Bullied and The Brave/Assets/Scripts/CemaraController.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class CemaraController : MonoBehaviour
 {
+    int timer;
+
+    private void Start()
+    {
+        timer = 90;
+    }
+
+    private void FixedUpdate()
+    {
+        if (timer > 0) timer--;
+    }
+
     private void Update()
     {
-        GetComponent<Transform>().position = new Vector3(
+        
+        if (timer <= 0)
+        {
+            GetComponent<Transform>().position = new Vector3(
             GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position.x,
             GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position.y,
             -10);
+        }
+        
     }
 }
